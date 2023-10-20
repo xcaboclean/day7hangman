b262f1026fadff1325d877b5e07a78eabee99ffe
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2023-10-20 03:06:44 +0000

    fix - counting lives

--- main.py
+++ main.py
@@ -61,15 +61,13 @@ stages = ['''
 
 end_of_game = False
 lives = 6
+letter_found = False
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
-    #TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-    #Set 'lives' to equal 6.
-
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
 
 #Create blanks
 display = []
@@ -82,24 +80,16 @@ while not end_of_game:
   #Check guessed letter
   for position in range(word_length):
     letter = chosen_word[position]
-  # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
     if letter == guess:
       display[position] = letter
+      letter_found = True
     else:
       lives -= 1
-      if not lives:
-        print("You lose!")
-              
-        #TODO-2: - If guess is not a letter in the chosen_word,
-        #Then reduce 'lives' by 1. 
-        #If lives goes down to 0 then the game should stop and it should print "You lose."
-
-        #Join all the elements in the list and turn it into a String.
-  print(f"{' '.join(display)}")
-
-        #Check if user has got all letters.
+  print(display)
+  print(stages[6 - lives])
   if "_" not in display:
     end_of_game = True
     print("You win.")
-
+  if lives == 0:
+    end_of_game = True 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
