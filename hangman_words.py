315e0113c80135673a9feb32be7ab950b1c6814c
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2023-10-21 01:48:29 +0000

    cleanning coments and print test

---
+++ hangman_words.py
@@ -0,0 +1,1 @@
+word_list = ["avocado","pineapple", "acai", "barbadoscherry", "plum", "prunes", "blackberry", "araza", "atemoya", "jellypalm", "banana", "apple", "grape", "orange", "strawberry", "grapefruit", "peach", "pear", "raspberry", "cherry","cocoa", "coconut", "cupuacu", "apricot", "soursop", "jackfruit", "cupuacu", "guava", "fig", "braziliangrape", "gooseberry","persimon"]
--- main.py
+++ main.py
@@ -1,81 +1,21 @@
-    #Step 4
-
 import random
+from hangman_words import word_list
+from hangman_art import stages,logo
 
-stages = ['''
-      +---+
-      |   |
-      O   |
-     /|\  |
-     / \  |
-          |
-    =========
-    ''', '''
-      +---+
-      |   |
-      O   |
-     /|\  |
-     /    |
-          |
-    =========
-    ''', '''
-      +---+
-      |   |
-      O   |
-     /|\  |
-          |
-          |
-    =========
-    ''', '''
-      +---+
-      |   |
-      O   |
-     /|   |
-          |
-          |
-    =========''', '''
-      +---+
-      |   |
-      O   |
-      |   |
-          |
-          |
-    =========
-    ''', '''
-      +---+
-      |   |
-      O   |
-          |
-          |
-          |
-    =========
-    ''', '''
-      +---+
-      |   |
-          |
-          |
-          |
-          |
-    =========
-    ''']
+print(logo)
 
 end_of_game = False
 lives = 6
 letter_found = False
-word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
 
 #Create blanks
 display = []
 for _ in range(word_length):
   display += "_"
 
-#Testing code
-print(f"Psst, the solution is {chosen_word}.")
 #loop for read a guess
 while not end_of_game:
   guess = input("Guess a letter: ").lower()
@@ -102,5 +42,3 @@ while not end_of_game:
   if lives == 0:
     end_of_game = True
     print("You lose.")
-    
-    #TODO-3: - print the ASCII art from 'stages' thae aí, mulheres...t corresponds to the current number of 'lives' the user has remaining.
